9a964de55f44a68f364ab83f90c00814a3946e89
Usman <usmanjamil8641@gmail.com>
2025-03-16 08:10:07 +0500

    url fixed

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,6 @@ import { io } from "socket.io-client";
 
 const BACKEND_URL = "https://aesthetic-lorianna-teamsphere-b28ca5af.koyeb.app"; // Your Koyeb backend URL
 // const BACKEND_URL = "http://localhost:3001"; // Your Koyeb backend URL
-
 // Connect to the backend (change to your backend URL)
 const socket = io(BACKEND_URL, {
   transports: ["websocket"], // Ensures WebSocket connection
